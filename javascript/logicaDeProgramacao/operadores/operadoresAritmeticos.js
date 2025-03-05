3e6cef58120fc38e7f6b1c645e7690797f103de3
adrianfagundes2024 <adrianfagundes2024@gmail.com>
2025-03-05 09:58:25 -0300

    feat: add operadores directory with basic examples

---
+++ javascript/logicaDeProgramacao/operadores/operadoresAritmeticos.js
@@ -0,0 +1,55 @@
+/*
+ Operadores aritméticos:
+ +      adição
+ -      subtração
+ *      multiplicação
+ /      divisão
+ **     potencia
+ ++     incremento  seria a mesma coisa que     x = x + 1
+ --     decremento  seria a mesma coisa que     y = y - 1
+*/
+let num1 = 4
+let num2 = 3
+
+
+
+//adição
+console.log("\nadição")
+console.log(`${num1} + ${num2} = ${num1 + num2}`)
+
+
+//subtração
+console.log("\nsubtração")
+console.log(`${num1} - ${num2} = ${num1 - num2}`)
+
+
+//multiplicação
+console.log("\nmultiplicação")
+console.log(`${num1} * ${num2} = ${num1 * num2}`)
+
+
+//divisão
+console.log("\ndivisão")
+console.log(`${num1} / ${num2} = ${num1 / num2}`)
+
+
+//módulo(resto de uma operação de divisão)
+console.log("\nmódulo")
+console.log(`${num1} % ${num2} = ${num1 % num2}`)
+
+
+//potencia
+console.log("\npotencia")
+console.log(`${num1} ** ${num2} = ${num1 ** num2}`)
+
+
+//decremento
+console.log("\ndecremento")
+console.log(`--${num1} = ${--num1}`)
+
+
+//incremento
+console.log("\nincremento")
+console.log(`++${num1} = ${++num1}`)
+
+// usar o ++ ou --, antes da variavel executa antes e vice-versa
